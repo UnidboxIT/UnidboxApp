4e4405f0378c0b28022752c6d987c97feed7b1b3
susandishwe21 <susandishwe2598@gmail.com>
2024-04-29 15:30:28 +0630

    update

--- lib/views/screens/profile_screens/widgets/each_list_tile_widget.dart
+++ lib/views/screens/profile_screens/widgets/each_list_tile_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:unidbox_app/controllers/notification_controller.dart';
-import 'package:unidbox_app/utils/constant/app_color.dart';
 import 'package:unidbox_app/views/widgets/text_widget.dart';
 
 Widget eachListTileWidget(
