e06eeaed20468c31bf33edb83d342756cb90394e
susandishwe21 <susandishwe2598@gmail.com>
2024-05-28 15:04:37 +0630

    refactor inventroy

--- lib/controllers/home_controllers/product_controller.dart
+++ lib/controllers/home_controllers/product_controller.dart
@@ -4,10 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:unidbox_app/services/home/product_service.dart';
-import 'package:unidbox_app/utils/commons/common_method.dart';
 import 'package:unidbox_app/utils/commons/super_print.dart';
 import 'package:http/http.dart' as http;
-import 'package:unidbox_app/views/screens/home/inventory_screens/inventory_tracker/product_screen.dart';
 import '../../models/home/product.dart';
 import '../../models/home/uom.dart';
 
@@ -46,26 +44,6 @@ class ProductController extends GetxController {
   @override
   onInit() {
     super.onInit();
-    scrollController.addListener(() async {
-      if (scrollController.position.pixels >=
-          scrollController.position.maxScrollExtent) {
-        if (xDataExit) {
-          if (!xLoading) {
-            xLoading = true;
-            superPrint(xLoading);
-            update();
-            ++pageNumber;
-            await getAllProductsByCategoryID();
-            await Future.delayed(const Duration(seconds: 1));
-            xLoading = false;
-            update();
-          }
-        } else {
-          xDataExit = true;
-          update();
-        }
-      }
-    });
     uomScrollController.addListener(() async {
       if (uomScrollController.position.pixels >=
           uomScrollController.position.maxScrollExtent) {
@@ -157,114 +135,6 @@ class ProductController extends GetxController {
     update();
   }
 
-  Future<void> pickImage(ImageSource source) async {
-    try {
-      final pickedFile = await picker.pickImage(source: source);
-      if (pickedFile != null) {
-        imageFile = File(pickedFile.path);
-        base64Image = await imageToBase64(imageFile);
-        Get.back();
-      } else {
-        superPrint('No image selected.');
-      }
-    } catch (e) {
-      superPrint('Error picking image: $e');
-    }
-    update();
-  }
-
-  Future<String> imageToBase64(File imageFile) async {
-    List<int> imageBytes = await imageFile.readAsBytes();
-    String base64Image = base64Encode(imageBytes);
-    return base64Image;
-  }
-
-  Future<void> searchProductByText(String query,
-      {bool isInventoryTracker = false}) async {
-    searchProductsList.clear();
-    if (query.isEmpty) {
-      searchProductsList.addAll(productList);
-    } else {
-      searchProductsList = productList
-          .where((element) =>
-              element.name.toLowerCase().contains(query.toLowerCase()) ||
-              element.barcode.toLowerCase().contains(query.toLowerCase()) ||
-              element.quantity
-                  .toString()
-                  .toLowerCase()
-                  .contains(query.toLowerCase()))
-          .toList();
-    }
-
-    update();
-  }
-
-  Future<void> getAllProductsByCategoryID() async {
-    superPrint(categoryID);
-    if (productList.isNotEmpty) {
-      isProductLoading = true;
-      update();
-    }
-    try {
-      http.Response response =
-          await ProductService.products(categoryID, pageNumber);
-      var result = jsonDecode(response.body);
-
-      if (result['result']['code'] == 200) {
-        Iterable dataList = result['result']['records'];
-        for (var element in dataList) {
-          productList.add(Products.fromJson(element));
-        }
-        for (var data in productList) {
-          searchProductsList.add(data);
-        }
-        if (dataList.isEmpty) {
-          xDataExit = false;
-          update();
-        }
-      }
-    } catch (e) {
-      superPrint(e.toString());
-    }
-    isProductLoading = false;
-    update();
-  }
-
-  Future<void> scanProductByBarCode(String barcode) async {
-    if (productList.isEmpty) {
-      isProductLoading = true;
-      update();
-    }
-    try {
-      http.Response response = await ProductService.scanProduct(barcode);
-      var result = jsonDecode(response.body);
-      productList.clear();
-      searchProductsList.clear();
-      superPrint(result);
-      if (result['result']['code'] == 200) {
-        Iterable dataList = result['result']['records'];
-        if (dataList.isNotEmpty) {
-          for (var element in dataList) {
-            productList.add(Products.fromJson(element));
-          }
-          for (var data in productList) {
-            searchProductsList.add(data);
-          }
-          Get.to(() => ProductScreen(
-                name: searchProductsList[0].categoryIdList[1],
-                parentID: searchProductsList[0].categoryIdList[0].toString(),
-              ));
-        } else {
-          //CommonMethods.customizedAlertDialog("No Product!", isPop: false);
-        }
-      }
-    } catch (e) {
-      superPrint(e.toString());
-    }
-    isProductLoading = false;
-    update();
-  }
-
   clearSelectedImage() {
     imageFile = File("");
     base64Image = "";
@@ -286,7 +156,7 @@ class ProductController extends GetxController {
         txtCost.text,
         varietyValueMap.values.toList(),
       );
-      var result = jsonDecode(response.body);
+      // var result = jsonDecode(response.body);
       // CommonMethods.customizedAlertDialog(result['result']['message'],
       //     isPop: false);
       clearProductVariety();
