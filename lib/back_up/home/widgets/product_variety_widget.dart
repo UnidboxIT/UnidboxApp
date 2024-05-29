e6b781b7d364c49683a115c95e6f2f24855499ae
susandishwe21 <susandishwe2598@gmail.com>
2024-05-29 18:17:19 +0630

    update

--- lib/back_up/home/widgets/product_variety_widget.dart
+++ lib/back_up/home/widgets/product_variety_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:unidbox_app/back_up/controllers/home_controllers/product_controller.dart';
-import 'package:unidbox_app/utils/commons/common_method.dart';
 import 'package:unidbox_app/utils/constant/app_color.dart';
 import 'package:unidbox_app/views/widgets/text_widget.dart';
 import '../create_product_widget/each_text_field_widget.dart';
